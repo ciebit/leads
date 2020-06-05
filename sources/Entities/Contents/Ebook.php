bb7c75db014cfca6bc9d8818976bfad6c26a0a95
Johnny Saymon <saymon@ciebit.com.br>
2020-06-05 08:19:05 -0300

    feate(content): ebook entity and testing

---
+++ sources/Entities/Contents/Ebook.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ciebit\Leads\Entities\Contents;
+
+use Ciebit\Leads\Entities\Contents\Content;
+use Ciebit\Leads\Entities\Contents\Status;
+use Ciebit\Leads\Entities\Contributors\Collection as ContributorsCollection;
+use Ciebit\Leads\Entities\DateTime\DateTime;
+use Ciebit\Leads\Entities\Topics\Collection as TopicsCollection;
+use DateTimeInterface;
+
+final class Ebook implements Content
+{
+    public const TYPE = 'ebook';
+
+    private ContributorsCollection $authors;
+    private string $content;
+    private string $coverId;
+    private DateTime $dateTime;
+    private string $description;
+    private string $formLink;
+    private string $id;
+    private string $slug;
+    private Status $status;
+    private string $title;
+    private TopicsCollection $topics;
+
+    public function __construct(
+        string $title,
+        string $slug,
+        string $description,
+        string $content,
+        DateTime $dateTime,
+        TopicsCollection $topics,
+        ContributorsCollection $authors,
+        string $coverId,
+        Status $status,
+        string $formLink,
+        string $id = ''
+    ) {
+        $this->authors = $authors;
+        $this->content = $content;
+        $this->coverId = $coverId;
+        $this->dateTime = $dateTime;
+        $this->description = $description;
+        $this->formLink = $formLink;
+        $this->id = $id;
+        $this->slug = $slug;
+        $this->status = $status;
+        $this->title = $title;
+        $this->topics = $topics;
+    }
+
+    public function getAuthors(): ContributorsCollection
+    {
+        return clone $this->authors;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function getCoverId(): string
+    {
+        return $this->coverId;
+    }
+
+    public function getDateTime(): DateTime
+    {
+        return clone $this->dateTime;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getFormLink(): string
+    {
+        return $this->formLink;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function getStatus(): Status
+    {
+        return $this->status;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getTopics(): TopicsCollection
+    {
+        return clone $this->topics;
+    }
+
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'authors' => $this->getAuthors(),
+            'content' => $this->getContent(),
+            'coverId' => $this->getCoverId(),
+            'dateTime' => $this->getDateTime()->format(DateTimeInterface::W3C),
+            'description' => $this->getDescription(),
+            'formLink' => $this->getFormLink(),
+            'id' => $this->getId(),
+            'slug' => $this->getSlug(),
+            'status' => $this->getStatus()->getValue(),
+            'title' => $this->getTitle(),
+            'topics' => $this->getTopics(),
+        ];
+    }
+}
---
+++ tests/classes/Entities/Contents/EbookTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ciebit\Leads\Tests\Entities\Contents;
+
+use Ciebit\Leads\Entities\Contents\Status;
+use Ciebit\Leads\Entities\Contents\Ebook;
+use Ciebit\Leads\Entities\Contributors\Collection as ContributorsCollection;
+use Ciebit\Leads\Entities\DateTime\Defined as DateTimeDefined;
+use Ciebit\Leads\Entities\DateTime\Undefined as DateTimeUndefined;
+use Ciebit\Leads\Entities\Topics\Collection as TopicsCollection;
+use PHPUnit\Framework\TestCase;
+
+class EbookTest extends TestCase
+{
+    public function testBasic(): void
+    {
+        $topics = new TopicsCollection();
+        $authors = new ContributorsCollection();
+        $dateTime = new DateTimeUndefined();
+        $status = new Status(Status::ACTIVE);
+
+        $ebook = new Ebook(
+            'Title',
+            'title-slug',
+            'Description',
+            'Content',
+            $dateTime,
+            $topics,
+            $authors,
+            '5',
+            $status,
+            'http://form-link.com'
+        );
+
+        $this->assertEquals('Title', $ebook->getTitle());
+        $this->assertEquals('title-slug', $ebook->getSlug());
+        $this->assertEquals('Description', $ebook->getDescription());
+        $this->assertEquals('Content', $ebook->getContent());
+        $this->assertEquals($dateTime, $ebook->getDateTime());
+        $this->assertEquals($topics, $ebook->getTopics());
+        $this->assertEquals($authors, $ebook->getAuthors());
+        $this->assertEquals(5, $ebook->getCoverId());
+        $this->assertEquals($status, $ebook->getStatus());
+        $this->assertEquals('http://form-link.com', $ebook->getFormLink());
+    }
+
+    public function testJsonSerialize(): void
+    {
+        $data = [
+            'id' => '2',
+            'title' => 'Title',
+            'slug' => 'title-slug',
+            'description' => 'Description',
+            'content' => 'Content',
+            'dateTime' => '2020-05-26T19:20:42-03:00',
+            'topics' => [],
+            'authors' => [],
+            'coverId' => '5',
+            'status' => Status::ACTIVE,
+            'formLink' => 'http://form-link.com'
+        ];
+
+        $ebook = new Ebook(
+            $data['title'],
+            $data['slug'],
+            $data['description'],
+            $data['content'],
+            new DateTimeDefined($data['dateTime']),
+            new TopicsCollection(),
+            new ContributorsCollection(),
+            $data['coverId'],
+            new Status($data['status']),
+            $data['formLink'],
+            $data['id']
+        );
+
+        $this->assertJsonStringEqualsJsonString(
+            json_encode($data),
+            json_encode($ebook)
+        );
+    }
+}
